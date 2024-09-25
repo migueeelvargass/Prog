c07ba7726acda4da0b429f64d675233b232e45cb
migueeelvargass <miguelv2504@gmail.com>
2024-09-25 09:00:52 -0500

    Primer Commit

---
+++ Business/INV/MarcaBL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoDemo.Business.INV
+{
+    internal class MarcaBL
+    {
+    }
+}
---
+++ DTO/INV/MarcaDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoDemo.DTO.INV
+{
+    public class MarcaDTO
+    {
+        // (?) Significa que ese campo puede ser nulo
+        public int Id {  get; set; }
+        public string Descripcion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public string? UsuarioCrea {  get; set; }
+
+        // Campos opcionales
+        public DateTime? FechaModificacion { get; set; }
+        public string? UsuarioModifica {  get; set; }
+        public bool Estado { get; set; } = false;
+    }
+}
